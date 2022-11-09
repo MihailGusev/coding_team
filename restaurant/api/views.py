f864307e6941c527871a21196336012a17cf2026
Mikhail <hookman007@gmail.com>
2022-11-09 14:35:44 +0300

    View for foods

--- restaurant/api/views.py
+++ restaurant/api/views.py
@@ -1,9 +1,39 @@
+from typing import OrderedDict
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .serializers import FoodSerializer, FoodListSerializer
+from .serializers import FoodListSerializer
 from core.models import Food, FoodCategory
+
 
 @api_view(['GET'])
 def get_foods(request):
-    return Response()
+    # setup()
+    foods = Food.objects.filter(is_publish=True)
+    food_codes = set(f.internal_code for f in foods)
+    categories = FoodCategory.objects.filter(food__in=foods).distinct()
+    data = FoodListSerializer(categories, many=True).data
+    for category in data:
+        foods: OrderedDict = category['foods']
+        for i in range(len(foods)-1, -1, -1):
+            if foods[i]['internal_code'] not in food_codes:
+                foods.pop(i)
+
+    return Response(data)
+
+
+def setup():
+    # return
+    Food.objects.all().delete()
+    FoodCategory.objects.all().delete()
+
+    beverages = FoodCategory.objects.create(name_ru='Напитки', order_id=10)
+    cola = Food.objects.create(category=beverages, internal_code=100, code=1, name_ru='Чай', description_ru='Чай 100гр', cost=123)
+    tea = Food.objects.create(category=beverages, internal_code=200, code=2, name_ru='Кола', description_ru='Кола', cost=123)
+    cola.additional.set([tea])
+    Food.objects.create(category=beverages, internal_code=300, code=3, name_ru='Спрайт', description_ru='Спрайт', cost=123)
+    Food.objects.create(category=beverages, internal_code=400, code=4, name_ru='Байкал', description_ru='Байкал', cost=123, is_publish=False)
+
+    bakery = FoodCategory.objects.create(name_ru='Выпечка', order_id=20)
+    Food.objects.create(category=bakery, internal_code=500, code=5, name_ru='Багет', description_ru='Багет', cost=456, is_publish=False)
+    Food.objects.create(category=bakery, internal_code=600, code=6, name_ru='Пончик', description_ru='Пончик', cost=456, is_publish=False)
